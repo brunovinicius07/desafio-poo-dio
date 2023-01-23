8f48f4e2464d7422cf05b42e0ba68f336e7ed13d
brunovinicius07 <brunoviniciusoliveiratavares@gmail.com>
2023-01-23 20:09:45 -0300

    Criada as CLasses Curso e Mentoria

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Java");
+        curso1.setDescricao("Back-End");
+        curso1.setCargaHoraria(10);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Angular");
+        curso2.setDescricao("Front-End");
+        curso2.setCargaHoraria(8);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Java");
+        mentoria1.setDescricao("POO");
+        mentoria1.setData(LocalDate.now());
+
+        System.out.println(mentoria1);
     }
 }
